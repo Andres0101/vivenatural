370698de1136b2e1dc87d187e4a7b613b9198146
Andres0101 <andrew.0101@hotmail.com>
2016-05-19 00:50:11 -0500

    Change code to be able to use new firebase

--- app/src/main/java/com/example/andres_bonilla/ensayo/activity/Config.java
+++ app/src/main/java/com/example/andres_bonilla/ensayo/activity/Config.java
@@ -5,6 +5,6 @@ public class Config {
     public static final String GCM_SENDER_ID = "686329939471";*/
     /*public static final String BATCH_API_KEY = "AIzaSyCeqPr5lpq8xpHtDXguE_GamL_Ub8IU12U";
     public static final String GCM_SENDER_ID = "156977879962";*/
-    public static final String BATCH_API_KEY = "AIzaSyCOhLFKDD4Bw99XIt4lV98IBxX_KDsiOuw";
-    public static final String GCM_SENDER_ID = "156977879962";
+    public static final String BATCH_API_KEY = "AIzaSyD-iNhb2_pk5e4qulmu_Fjul-Np5vmLzcA";
+    public static final String GCM_SENDER_ID = "236350259608";
 }
